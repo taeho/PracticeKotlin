8badee39fa13c814568fc493064d1ab4eac360db
gopae02 <gopae02@gmail.com>
2019-09-20 23:58:46 +0900

    ## 19/09/20
    kotlin 중급 문법 연습

--- src/com/practice067/Main.kt
+++ src/com/practice067/Main.kt
@@ -1,9 +1,10 @@
 package com.practice067.ex_overriding_property
+// 프로퍼티를 오버라이딩 하기
 
-open class AAA
+open class AAA	// 프로퍼티 오버라이딩을 위한 open 키워드 사용 
 {
 	open var number = 10
-		get()
+		get() // number 프로퍼티의 getter 커스터마이징
 		{
 			println("AAA number Getter 호출됨")
 			return field
@@ -17,22 +18,22 @@ open class AAA
 
 class BBB : AAA()
 {
-	override var number: Int
+	override var number: Int	// AAA 클래스의 number 프로퍼티를 오버라이딩, 할때 overrrid 키워드 필요
 		get()
 		{
 			println("BBB number Getter 호출됨")
-			return super.number
+			return super.number		// super.number같이 하면 슈퍼클래스인 AAA클래스의 number 프로퍼티의 getter 호출
 		}
 		set(value)
 		{
 			println("BBB number Setter 호출됨")
-			super.number = value
+			super.number = value	// super.number = 표현식 같이 하면 슈퍼클래스인 AAA클래스의 number 프로퍼티의 Setter 호출
 		}
 }
 
 fun main(args: Array<String>)
 {
-	val test = BBB()
-	test.number = 5
-	test.number
+	val test = BBB()	// BBB 인스턴스 생성
+	test.number = 5		// BBB클래스에서 오버라이딩 한 number 프로퍼티의 Setter 호출
+	test.number			// BBB클래스에서 오버라이딩 한 number 프로퍼티의 Getter 호출
 }
